d6c4c2ace7a52a243505de63cc05aaa99abf31ca
Christian Maigaard Christensen <>
2018-11-28 08:15:17 +0100

    beginning of summonor name activity, transfer commit

---
+++ app/src/main/java/com/example/christianmaigaard/lolcompanion/EnterSummonerNameActivity.java
@@ -0,0 +1,13 @@
+package com.example.christianmaigaard.lolcompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterSummonerNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_summoner_name);
+    }
+}
